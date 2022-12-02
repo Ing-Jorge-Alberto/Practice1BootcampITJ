7209dee0d63873eb696ae2b0cbf037ec10dd3034
George <jorge.canales@dexcom.com>
2022-12-01 21:12:08 -0600

    Xpath and CSS selectors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,14 +12,39 @@ public class Main {
         /*FireFox driver*/
         System.setProperty("webdriver.geko.driver","/Users/jxc0622/Web Drivers/geckodriver");
         WebDriver driver = new FirefoxDriver();
+
         driver.get("https://oscarvalerio.github.io/seleniumExercises/practice1.html");
         String browserTitle = driver.getTitle();
         System.out.println(browserTitle);
         String browserURL=driver.getCurrentUrl();
         System.out.println(browserURL);
-        driver.get("https://www.facebook.com/");
+        //Thread.sleep(2000);
+        //driver.get("https://www.facebook.com/");
+        //driver.navigate().back();
+        driver.findElement(By.id("userField")).sendKeys("JACUserName");
+        driver.findElement(By.linkText("Exercise 1 (Easy)")).click();
         driver.navigate().back();
-        driver.close();
+        //driver.findElement(By.className("form-control")).sendKeys("JACPassword");
+        driver.findElement(By.id("passwordField")).sendKeys("JACPassword");
+        driver.findElement(By.name("buttonToSubmit")).click();
+        //driver.close();
         //driver.navigate don´t wait to the page ends to load, and driver.get yes
+
+        //Xpath structure
+        //objectType[@attribute='value']
+
+        //CSS structure
+        //  objectType[attribute='value']
+
+        driver.findElement(By.xpath("//input")).sendKeys("Hello");
+
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("xpathUsername");
+        driver.findElement(By.xpath("//input[@id='sendCode']")).click();
+        //Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='sendCode']")).click();
+        //Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='sendCode']")).click();
+        //<input type="text" class="form-control" id="userField" placeholder="Username">
+        driver.close();
     }
 }
